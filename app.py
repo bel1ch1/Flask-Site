8b867829521136c6eb67cf963edaeef536369a9d
AndreyZ <andreyzv2525@gmail.com>
2023-12-17 18:36:17 +0300

    end

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, errors
-from app import routes
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
@@ -19,10 +24,11 @@ def my_social():
     return render_template("my_social.html")
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('bad_404.html'), 404
+def buttonLike():
+    with open('button_logs.txt', 'a') as file:
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file.write(f'Button pressed at {timestamp}\n')
+    return 'Button press logged'
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
